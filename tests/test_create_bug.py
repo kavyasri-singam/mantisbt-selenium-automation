3f3bde6b93f658c669b8d0ab5cd221a24fa4267f
Kavya Sri <kadambari1102000@gmail.com>
2025-06-07 12:41:04 +0530

    🐛 Fix: Added missing import for WebDriverWait

--- tests/test_create_bug.py
+++ tests/test_create_bug.py
@@ -2,6 +2,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 @pytest.fixture
 def driver():
